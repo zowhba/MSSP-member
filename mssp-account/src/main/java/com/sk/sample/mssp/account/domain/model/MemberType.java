f67d9c61050a84ce3ac5f203bc0cf2e6cc2066b1
zerohkang <kangyh700@gmail.com>
2019-06-26 16:43:36 +0900

    11

---
+++ mssp-account/src/main/java/com/sk/sample/mssp/account/domain/model/MemberType.java
@@ -0,0 +1,6 @@
+package com.sk.sample.mssp.account.domain.model;
+
+public enum MemberType {
+	SELLER,
+	BUYER
+}
---
+++ mssp-account/src/main/java/com/sk/sample/mssp/account/domain/model/MembershipLevelType.java
@@ -0,0 +1,7 @@
+package com.sk.sample.mssp.account.domain.model;
+
+public enum MembershipLevelType {
+	VIP,
+	GOLD,
+	SILVER
+}
