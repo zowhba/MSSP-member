876ff77d0911e4967f9addff160092708c8c4396
zerohkang <kangyh700@gmail.com>
2019-06-26 22:45:36 +0900

    bb

--- src/main/java/com/sk/sample/mssp/member/domain/model/MembershipLevelType.java
+++
@@ -1,7 +0,0 @@
-package com.sk.sample.mssp.account.domain.model;
-
-public enum MembershipLevelType {
-	VIP,
-	GOLD,
-	SILVER
-}
--- target/generated-sources/java/com/sk/sample/mssp/member/domain/model/QMember.java
+++ target/generated-sources/java/com/sk/sample/mssp/member/domain/model/QMember.java
@@ -1,4 +1,4 @@
-package com.sk.sample.mssp.account.domain.model;
+package com.sk.sample.mssp.member.domain.model;
 
 import static com.querydsl.core.types.PathMetadataFactory.*;
 
@@ -11,16 +11,16 @@ import com.querydsl.core.types.dsl.PathInits;
 
 
 /**
- * QAccount is a Querydsl query type for Account
+ * QMember is a Querydsl query type for Member
  */
 @Generated("com.querydsl.codegen.EntitySerializer")
-public class QAccount extends EntityPathBase<Account> {
+public class QMember extends EntityPathBase<Member> {
 
-    private static final long serialVersionUID = -126402577L;
+    private static final long serialVersionUID = -1459227931L;
 
     private static final PathInits INITS = PathInits.DIRECT2;
 
-    public static final QAccount account = new QAccount("account");
+    public static final QMember member = new QMember("member");
 
     public final com.sk.sample.mssp.shared.base.QAbstractEntity _super = new com.sk.sample.mssp.shared.base.QAbstractEntity(this);
 
@@ -31,29 +31,33 @@ public class QAccount extends EntityPathBase<Account> {
     //inherited
     public final NumberPath<Long> id = _super.id;
 
-    public final EnumPath<MembershipLevelType> membershipLevelType = createEnum("membershipLevelType", MembershipLevelType.class);
-
     public final EnumPath<MemberType> memberType = createEnum("memberType", MemberType.class);
 
     public final StringPath name = createString("name");
 
-    public QAccount(String variable) {
-        this(Account.class, forVariable(variable), INITS);
+    public final StringPath password = createString("password");
+
+    public final StringPath telephone = createString("telephone");
+
+    public final StringPath userId = createString("userId");
+
+    public QMember(String variable) {
+        this(Member.class, forVariable(variable), INITS);
     }
 
-    public QAccount(Path<? extends Account> path) {
+    public QMember(Path<? extends Member> path) {
         this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
     }
 
-    public QAccount(PathMetadata metadata) {
+    public QMember(PathMetadata metadata) {
         this(metadata, PathInits.getFor(metadata, INITS));
     }
 
-    public QAccount(PathMetadata metadata, PathInits inits) {
-        this(Account.class, metadata, inits);
+    public QMember(PathMetadata metadata, PathInits inits) {
+        this(Member.class, metadata, inits);
     }
 
-    public QAccount(Class<? extends Account> type, PathMetadata metadata, PathInits inits) {
+    public QMember(Class<? extends Member> type, PathMetadata metadata, PathInits inits) {
         super(type, metadata, inits);
         this.address = inits.isInitialized("address") ? new com.sk.sample.mssp.shared.domain.QAddress(forProperty("address")) : null;
     }
